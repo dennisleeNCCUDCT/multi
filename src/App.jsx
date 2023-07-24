cbdc2e4907da8396aedb267c4ca2deff731c27d9
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-24 12:07:26 +0800

    success

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/DU1Wl8CHxSI6iOUo/scene.splinecode" />
+  );
+}
