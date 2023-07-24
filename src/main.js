bec9bda8055b53e2a04436025d7ac473c26af342
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-24 11:56:36 +0800

    initial

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/DU1Wl8CHxSI6iOUo/scene.splinecode');
